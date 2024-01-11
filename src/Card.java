f72c867d1e3a8059bb0acea1e402dafcbe793229
syddyd <sydney.dacks@gmail.com>
2024-01-10 21:24:48 -0500

    card class encapsulated with joker

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,31 @@
 public class Card {
     //this is where i will write my best version of card, encapsulated and with a joker
+    public enum suit { SPADES, HEARTS, DIAMONDS, CLUBS, JOKER }
+    public enum rank { ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, JOKER }
+
+    private suit cSuit;
+    private rank cRank;
+    public Card (suit s, rank r){
+        if (s == suit.JOKER || r == rank.JOKER){
+            cSuit = suit.JOKER;
+            cRank = rank.JOKER;
+        } else {
+            cSuit = s;
+            cRank = r;
+        }
+    }
+
+    public suit getSuit(){
+        return cSuit;
+    }
+
+    public rank getRank(){
+        return cRank;
+    }
+
+    public String ToString(){
+        if (getSuit() == suit.JOKER)
+            return "JOKER";
+        return getRank().toString()+" of "+getSuit().toString();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BoolCard myCard = new BoolCard("diamond", 13);
         System.out.println(myCard.getRank()+" of "+myCard.getSuit()+"s");
+
+        Card card = new Card(Card.suit.DIAMONDS, Card.rank.JOKER);
+        System.out.println(card.ToString());
     }
 }
